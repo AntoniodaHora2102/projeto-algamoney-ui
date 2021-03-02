656dc04c81b2f54dddf7e20a8e5e71505e737f52
AntoniodaHora2102 <antonio_dahora@hotmail.com>
2021-03-02 18:31:45 -0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: ' https://projetofrontendschool.herokuapp.com/' //vamos utilizar o protocolo em https em producao
+  apiUrl: 'https://projetofrontendschool.herokuapp.com' //vamos utilizar o protocolo em https em producao
   //apiUrl: 'http://localhost:8080' // teste direto na máquina
 };
